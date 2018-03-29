d60b2b5ab17d3d3cb40ff33af88c950cdc6f2760
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-03-29 13:55:34 +0100

    cleaned out console.logs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,12 +7,16 @@ const env = process.env.NODE_ENV || 'development'
 mongoose.Promise = global.Promise
 if (env === 'development') {
   mongoose.connect(process.env.DEV_DB).then(() => {
-    console.log('connected');
-  }, err => console.log(err))
+    return 'connected';
+  }, err => err)
 } else if (env === 'test') {
-  mongoose.connect(process.env.TEST_DB)
+  mongoose.connect(process.env.TEST_DB).then(() => {
+    return 'connected';
+  }, err => err)
 } else {
-  mongoose.connect(process.env.PROD_DB)
+  mongoose.connect(process.env.PROD_DB).then(() => {
+    return 'connected';
+  }, err => err)
 }
 
 module.exports = {
